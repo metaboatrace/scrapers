9d9c1d4bc24c86241d3da957e7bdce2c1efe7767
Johndoe <56298669+togane@users.noreply.github.com>
2022-08-08 20:48:23 +0900

    implement event entries scraper for official v1707

---
+++ boatrace/official/v1707/scrapers/tests/test_event_entries.py
@@ -0,0 +1,45 @@
+import os
+
+from boatrace.models.gender import Gender
+from boatrace.models.racer_rank import RacerRank
+from boatrace.official.v1707.scrapers.event_entries import (
+    scrape_pre_inspection_information,
+)
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_scrape_a_pre_inspection_information():
+    file_path = os.path.normpath(
+        os.path.join(
+            base_path, "./fixtures/pre_inspection_information/20151112_23#.html"
+        )
+    )
+    with open(file_path, mode="r") as file:
+        data = scrape_pre_inspection_information(file)
+
+    assert len(data) == 44
+    assert data[0].__dict__ == {
+        "racer_registration_number": 3470,
+        "racer_last_name": "新田",
+        "racer_first_name": "芳美",
+        "racer_rank": RacerRank.A1,
+        "motor_number": 70,
+        "quinella_rate_of_motor": 61.6,
+        "boat_number": 35,
+        "quinella_rate_of_boat": 39.2,
+        "anterior_time": 7.07,
+        "racer_gender": Gender.FEMALE,
+    }
+    assert data[-1].__dict__ == {
+        "racer_registration_number": 3518,
+        "racer_last_name": "倉田",
+        "racer_first_name": "郁美",
+        "racer_rank": RacerRank.A2,
+        "motor_number": 44,
+        "quinella_rate_of_motor": 20.3,
+        "boat_number": 36,
+        "quinella_rate_of_boat": 31.7,
+        "anterior_time": 6.96,
+        "racer_gender": Gender.FEMALE,
+    }
