b9b6446b69f257af0ec9c1508130a57125a19fcf
Johndoe <56298669+togane@users.noreply.github.com>
2022-08-29 20:03:56 +0900

    add motor parts factory

--- boatrace/models/motor_parts.py
+++ boatrace/models/motor_parts.py
@@ -10,3 +10,26 @@ class MotorParts(Enum):
     CRANKSHAFT = auto()
     GEAR_CASE = auto()
     CARRIER_BODY = auto()
+
+
+class MotorPartsFactory:
+    @staticmethod
+    def create(name: str):
+        if "電気" in name:
+            return MotorParts.ELECTRICAL_SYSTEM
+        elif "キャブ" in name:
+            return MotorParts.CARBURETOR
+        elif "ピストン" == name:
+            return MotorParts.PISTON
+        elif "リング" in name:
+            return MotorParts.PISTON_RING
+        elif "シリンダ" in name:
+            return MotorParts.CYLINDER
+        elif "ギア" in name:
+            return MotorParts.GEAR_CASE
+        elif "ギヤ" in name:
+            return MotorParts.GEAR_CASE
+        elif "キャリ" in name:
+            return MotorParts.CARRIER_BODY
+        else:
+            raise ValueError
