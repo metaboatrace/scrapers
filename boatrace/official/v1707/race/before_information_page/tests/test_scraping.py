6d0a3afb92f4c8c9dc29b1168133328ff800a018
Johndoe <56298669+togane@users.noreply.github.com>
2022-09-18 13:50:45 +0900

    fix namespace of boat setting scraping module

--- boatrace/official/v1707/race/before_information_page/tests/test_scraping.py
+++ boatrace/official/v1707/race/before_information_page/tests/test_scraping.py
@@ -1,11 +1,15 @@
 import os
 from datetime import date
 
-from boatrace.models.stadium_tel_code import StadiumTelCode
+import pytest
+from boatrace.models import MotorParts, StadiumTelCode
+from boatrace.official.exceptions import DataNotFound
 from boatrace.official.v1707.race.before_information_page.scraping import (
+    BoatSetting,
     CircumferenceExhibitionRecord,
     RacerCondition,
     StartExhibitionRecord,
+    extract_boat_settings,
     extract_circumference_exhibition_records,
     extract_racer_conditions,
     extract_start_exhibition_records,
@@ -380,5 +384,275 @@ def test_extract_racer_conditions_including_race_absent_racer():
             racer_registration_number=3797,
             weight=58.3,
             adjust=0.0,
+        ),
+    ]
+
+
+def test_extract_boat_settings():
+    file_path = os.path.normpath(
+        os.path.join(base_path, "./fixtures/20151116_23#_1R.html")
+    )
+
+    with open(file_path, mode="r") as file:
+        data = extract_boat_settings(file)
+
+    assert data == [
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=1,
+            pit_number=1,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=1,
+            pit_number=2,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=1,
+            pit_number=3,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=1,
+            pit_number=4,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=1,
+            pit_number=5,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=1,
+            pit_number=6,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+    ]
+
+
+def test_extract_boat_settings_including_propeller_exchanges():
+    file_path = os.path.normpath(
+        os.path.join(base_path, "./fixtures/20180619_04#_4R.html")
+    )
+
+    with open(file_path, mode="r") as file:
+        data = extract_boat_settings(file)
+
+    assert data == [
+        BoatSetting(
+            race_holding_date=date(2018, 6, 19),
+            stadium_tel_code=StadiumTelCode.HEIWAJIMA,
+            race_number=4,
+            pit_number=1,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2018, 6, 19),
+            stadium_tel_code=StadiumTelCode.HEIWAJIMA,
+            race_number=4,
+            pit_number=2,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2018, 6, 19),
+            stadium_tel_code=StadiumTelCode.HEIWAJIMA,
+            race_number=4,
+            pit_number=3,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2018, 6, 19),
+            stadium_tel_code=StadiumTelCode.HEIWAJIMA,
+            race_number=4,
+            pit_number=4,
+            tilt=-0.5,
+            is_new_propeller=True,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2018, 6, 19),
+            stadium_tel_code=StadiumTelCode.HEIWAJIMA,
+            race_number=4,
+            pit_number=5,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2018, 6, 19),
+            stadium_tel_code=StadiumTelCode.HEIWAJIMA,
+            race_number=4,
+            pit_number=6,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+    ]
+
+
+def test_extract_boat_settings_including_absent_racers():
+    file_path = os.path.normpath(
+        os.path.join(base_path, "./fixtures/20151116_03#_11R.html")
+    )
+
+    with open(file_path, mode="r") as file:
+        data = extract_boat_settings(file)
+
+    assert data == [
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.EDOGAWA,
+            race_number=11,
+            pit_number=2,
+            tilt=0,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.EDOGAWA,
+            race_number=11,
+            pit_number=3,
+            tilt=0,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.EDOGAWA,
+            race_number=11,
+            pit_number=4,
+            tilt=0,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.EDOGAWA,
+            race_number=11,
+            pit_number=5,
+            tilt=0,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.EDOGAWA,
+            race_number=11,
+            pit_number=6,
+            tilt=0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+    ]
+
+
+def test_extract_boat_settings():
+    file_path = os.path.normpath(
+        os.path.join(base_path, "./fixtures/20200630_12#_12R.html")
+    )
+
+    with open(file_path, mode="r") as file:
+        with pytest.raises(DataNotFound):
+            extract_boat_settings(file)
+
+
+def test_scrape_boat_settings_including_motor_parts_exchanges():
+    file_path = os.path.normpath(
+        os.path.join(base_path, "./fixtures/20151116_23#_12R.html")
+    )
+
+    with open(file_path, mode="r") as file:
+        data = extract_boat_settings(file)
+
+    assert data == [
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=12,
+            pit_number=1,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=12,
+            pit_number=2,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=12,
+            pit_number=3,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=12,
+            pit_number=4,
+            tilt=0,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=12,
+            pit_number=5,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[
+                (MotorParts.PISTON, 2),
+                (MotorParts.PISTON_RING, 3),
+                (MotorParts.ELECTRICAL_SYSTEM, 1),
+                (MotorParts.GEAR_CASE, 1),
+            ],
+        ),
+        BoatSetting(
+            race_holding_date=date(2015, 11, 16),
+            stadium_tel_code=StadiumTelCode.KARATSU,
+            race_number=12,
+            pit_number=6,
+            tilt=-0.5,
+            is_new_propeller=False,
+            motor_parts_exchanges=[],
         ),
     ]
