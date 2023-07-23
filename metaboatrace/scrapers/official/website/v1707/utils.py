e761db844a897f342a0f5dc6836fb7118b9fbe7b
Johndoe <56298669+togane@users.noreply.github.com>
2023-07-23 12:52:39 +0900

    Fix namesapces

--- metaboatrace/scrapers/official/website/v1707/utils.py
+++ metaboatrace/scrapers/official/website/v1707/utils.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from boatrace.models.stadium_tel_code import StadiumTelCode
+from metaboatrace.models.stadium_tel_code import StadiumTelCode
 
 
 def format_stadium_tel_code_for_query_string(stadium_tel_code: StadiumTelCode):
