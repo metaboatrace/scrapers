181df6531d8ea61ea8e18d8433107201bc6b41d6
Johndoe <56298669+togane@users.noreply.github.com>
2023-08-05 12:19:50 +0900

    Fix racer scraping

--- metaboatrace/scrapers/official/website/v1707/pages/racer/profile_page/scraping.py
+++ metaboatrace/scrapers/official/website/v1707/pages/racer/profile_page/scraping.py
@@ -3,29 +3,11 @@ from dataclasses import dataclass
 from datetime import date
 from typing import IO
 
-from boatrace.models import Branch, Prefecture, RacerRank
-from boatrace.official.factories import PrefectureFactory
-from boatrace.official.v1707.decorators import no_content_handleable
 from bs4 import BeautifulSoup
+from metaboatrace.models.racer import Racer, RacerRank
+from metaboatrace.models.region import Branch, BranchFactory, PrefectureFactory
 
-
-# note: 体重も級別も一応保持する
-# 体重は節間日次で変動し、レースの直前情報でレース時の最新情報は取得できる
-# 級別も成績に応じて期毎に改められる
-# ただ、取得できるデータ（特にコストもかからないもの）は保持しておいた方がライブラリとしての汎用性が高くなるため持っておく
-# 血液型は流石にいらないと思うので取ってない
-@dataclass(frozen=True)
-class Racer:
-    last_name: str
-    first_name: str
-    registration_number: int
-    birth_date: date
-    height: int
-    weight: float
-    branch_prefecture: Branch
-    born_prefecture: Prefecture
-    term: int
-    current_rating: RacerRank
+from metaboatrace.scrapers.official.website.v1707.decorators import no_content_handleable
 
 
 @no_content_handleable
@@ -42,23 +24,20 @@ def extract_racer_profile(file: IO) -> Racer:
 
     if m := re.match(r"(\d{3})cm", dd_list[2].get_text()):
         height = int(m.group(1))
-    if m := re.match(r"(\d{2})kg", dd_list[3].get_text()):
-        weight = int(m.group(1))
-    branch_prefecture = Branch(PrefectureFactory.create(dd_list[5].get_text()))
+    branch = Branch(BranchFactory.create(dd_list[5].get_text()))
     born_prefecture = PrefectureFactory.create(dd_list[6].get_text())
     if m := re.match(r"(\d{2,3})期", dd_list[7].get_text()):
         term = int(m.group(1))
-    racer_rank = RacerRank(dd_list[8].get_text()[:2])
+    racer_rank = RacerRank.from_string(dd_list[8].get_text()[:2])
 
     return Racer(
+        registration_number=registration_number,
         last_name=last_name,
         first_name=first_name,
-        registration_number=registration_number,
+        term=term,
         birth_date=birth_date,
         height=height,
-        weight=weight,
-        branch_prefecture=branch_prefecture,
         born_prefecture=born_prefecture,
-        term=term,
+        branch=branch,
         current_rating=racer_rank,
     )
