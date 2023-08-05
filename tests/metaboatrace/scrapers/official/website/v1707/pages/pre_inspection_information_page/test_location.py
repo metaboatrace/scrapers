0b28d5b7234ef4a5a5354d82efb9cb82fc9c695a
Johndoe <56298669+togane@users.noreply.github.com>
2023-08-05 09:16:36 +0900

    Fix event entry page location module

--- tests/metaboatrace/scrapers/official/website/v1707/pages/pre_inspection_information_page/test_location.py
+++ tests/metaboatrace/scrapers/official/website/v1707/pages/pre_inspection_information_page/test_location.py
@@ -1,12 +1,13 @@
 from datetime import date
 
-from boatrace.models import StadiumTelCode
-from boatrace.official.v1707.pages.pre_inspection_information_page.location import (
+from metaboatrace.models.stadium import StadiumTelCode
+
+from metaboatrace.scrapers.official.website.v1707.pages.pre_inspection_information_page.location import (
     create_event_entry_page_url,
 )
 
 
-def test_create_event_entry_page_url():
+def test_create_event_entry_page_url() -> None:
     assert (
         create_event_entry_page_url(StadiumTelCode.HEIWAJIMA, date(2022, 9, 15))
         == "https://boatrace.jp/owpc/pc/race/rankingmotor?jcd=04&hd=20220915"
